f8a269846a0a88cc6b361859bee07af0e5877227
lukeodrobinak <ekul.luke12@gmail.com>
2024-02-26 17:45:59 -0700

    Added headers and comments

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,11 +1,20 @@
+/**
+ * BinarySearchTree.java
+ * Authors: Luke O’Drobinak; Initial File: Alainey Hellman
+ * Date: 2/26/2024
+ * Collaborators: N/A
+ * Citations: Oracle Java Documentation, Wikipedia.com, Stack Overflow
+ **/
 public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 
     public Node<Key, Value> root;
 
+    //Constructor
     public BinarySearchTree() {
         root = null;
     }
 
+    //Wrapper for Node's recursive size
     public int size() {
         return size(root);
     }
@@ -32,12 +41,14 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     //sets left/right or creates a new node appropriately, returns the
     //modified node n
     private Node<Key, Value> put(Node<Key, Value> n, Key key, Value val) {
+        //if the node is null, create a new node
         if (n == null) {
             return new Node<Key, Value>(key, val, 1);
         }
 
         int cmp = key.compareTo(n.getKey());
 
+        //if the key is less than the current node's key, go left and vice versa
         if (cmp < 0) {
             n.setLeft(put(n.getLeft(), key, val));
         } else if (cmp > 0) {
@@ -46,6 +57,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
             n.setValue(val);
         }
 
+        //set the size of the node
         n.setSize(size(n.getLeft()) + 1 + size(n.getRight()));
 
         return n;
@@ -59,12 +71,14 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     //recursive get
     //returns null if the key does not exist
     private Value get(Node<Key, Value> n, Key key) {
+        //if the node is null, return null
         if (n == null) {
             return null;
         }
 
         int cmp = key.compareTo(n.getKey());
 
+        //if the key is less than the current node's key, go left and vice versa
         if (cmp < 0) {
             return get(n.getLeft(), key);
         } else if (cmp > 0) {
@@ -85,23 +99,28 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     }
 
     private Node remove(Node<Key, Value> n, Key key) {
+        //if the node is null, return null
         if (n == null) {
             return null;
         }
 
         int cmp = key.compareTo(n.getKey());
 
+        //if the key is less than the current node's key, go left and vice versa
         if (cmp < 0) {
             n.setLeft(remove(n.getLeft(), key));
         } else if (cmp > 0) {
             n.setRight(remove(n.getRight(), key));
         } else {
+            //if the node has one child, replace it with the child
             if (n.getRight() == null) {
                 return n.getLeft();
             }
             if (n.getLeft() == null) {
                 return n.getRight();
             }
+
+            //if the node has two children, replace it with the min of the right
             Node min = min(n.getRight());
             min.setLeft(n.getLeft());
             n = n.getRight();
--- src/BinarySearchTreeDriver.java
+++ src/BinarySearchTreeDriver.java
@@ -1,6 +1,15 @@
+/**
+ * BinarySearchTreeDriver.java
+ * Authors: Luke O’Drobinak
+ * Date: 2/26/2024
+ * Collaborators: N/A
+ * Citations: Oracle Java Documentation, Wikipedia.com, Stack Overflow
+ **/
 public class BinarySearchTreeDriver {
     public static void main(String[] args) {
         BinarySearchTree<Integer, String> bst = new BinarySearchTree<>();
+
+        //put some values in the tree
         bst.put(5, "five");
         bst.put(3, "three");
         bst.put(7, "seven");
@@ -8,6 +17,8 @@ public class BinarySearchTreeDriver {
         bst.put(4, "four");
         bst.put(6, "six");
         bst.put(8, "eight");
+
+        //prints the values in the tree
         System.out.println(bst.get(5));
         System.out.println(bst.get(3));
         System.out.println(bst.get(7));
@@ -15,6 +26,8 @@ public class BinarySearchTreeDriver {
         System.out.println(bst.get(4));
         System.out.println(bst.get(6));
         System.out.println(bst.get(8));
+
+        //prints the check if the tree contains the values
         System.out.println(bst.contains(5));
         System.out.println(bst.contains(3));
         System.out.println(bst.contains(7));
@@ -22,6 +35,8 @@ public class BinarySearchTreeDriver {
         System.out.println(bst.contains(4));
         System.out.println(bst.contains(6));
         System.out.println(bst.contains(8));
+
+        //removes the values from the tree
         System.out.println(bst.remove(5));
         System.out.println(bst.remove(3));
         System.out.println(bst.remove(7));
@@ -29,6 +44,8 @@ public class BinarySearchTreeDriver {
         System.out.println(bst.remove(4));
         System.out.println(bst.remove(6));
         System.out.println(bst.remove(8));
+
+        //checks the get and contains methods after removing the values
         System.out.println(bst.get(5));
         System.out.println(bst.get(3));
         System.out.println(bst.get(7));
